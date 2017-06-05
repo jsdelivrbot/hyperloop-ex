9bc3ee5e4c7a4c83b0bc7e05b09e85207a2b0e9a
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-06-05 13:35:14 -0400

    helloworld component code

--- app/hyperloop/components/helloworld.rb
+++ app/hyperloop/components/helloworld.rb
@@ -1,38 +1,39 @@
 class Helloworld < Hyperloop::Component
 
-    state show_field: false
-    state field_value: ""
+  state show_field: false
+  state field_value: ""
 
-    render(DIV) do
-      show_button
-      DIV(class: 'formdiv') do
-        show_input
-        H1 { "#{state.field_value}" }
-      end if state.show_field
-    end
-
-    def show_button
-      BUTTON(class: 'btn btn-info') do
-        state.show_field ? "Click to hide HelloWorld input field" : "Click to show HelloWorld input field"
-      end.on(:click) { mutate.show_field }
-    end
-
-    def show_input
-
-      H4 do
-        SPAN{'Please type '}
-        SPAN(class: 'colored') {'Hello World'}
-        SPAN{' in the input field below :'}
-        BR {}
-        SPAN{'Or anything you want (^̮^)'}
-      end
-
-      INPUT(type: :text, class: 'form-control').on(:change) do |e|
-        mutate.field_value e.target.value
-      end
-    end
-
-    def show_text
+  render(DIV) do
+    show_button
+    DIV(class: 'formdiv') do
+      show_input
       H1 { "#{state.field_value}" }
+    end if state.show_field
+  end
+
+  def show_button
+    BUTTON(class: 'btn btn-info') do
+      state.show_field ? "Click to hide HelloWorld input field" : "Click to show HelloWorld input field"
+    end.on(:click) { mutate.show_field !state.show_field }
+  end
+
+  def show_input
+
+    H4 do
+      SPAN{'Please type '}
+      SPAN(class: 'colored') {'Hello World'}
+      SPAN{' in the input field below :'}
+      BR {}
+      SPAN{'Or anything you want (^̮^)'}
     end
+
+    INPUT(type: :text, class: 'form-control').on(:change) do |e|
+      mutate.field_value e.target.value
+    end
+  end
+
+  def show_text
+    H1 { "#{state.field_value}" }
+  end
+
 end
